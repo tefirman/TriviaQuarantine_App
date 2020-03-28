6963ed0f5b2920e483d2e35b5954963a93ae7124
Taylor Firman <tefirman@gmail.com>
2020-03-28 16:46:39 -0600

    Update Trivia Widget

--- TriviaWidget.py
+++ TriviaWidget.py
@@ -16,6 +16,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 import pandas as pd
 import datetime
+import plotly.graph_objs as go
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 SPREADSHEET_ID = '1EXnnJkVHObBsVdXg2JeTylRxVt9wQyEPZMIOhMJvkDU'
@@ -56,22 +57,25 @@ def add_response(team,question,answer,wager):
     put_values("Responses!A" + str(responses.shape[0] + 2) + ":F" + \
     str(responses.shape[0] + 2),[[team,str(datetime.datetime.now()),question,answer,wager,'Unscored']])
 
+standings = pd.DataFrame(columns=['Team Name','Score','Unscored'])
+fig = go.Figure(data=[go.Table(columnwidth=[100,50,50],header=dict(values=list(standings.columns),\
+fill_color='paleturquoise',align='center'),cells=dict(values=[standings['Team Name'],\
+standings['Score'],standings['Unscored']],fill_color='lavender',align='center'))])
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
 server = app.server
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.layout = html.Div([
     html.H1('Trivia: Quarantine Style'),
-    html.Div(id='question-container-button',children=current_question.Question.values[0]),
+    html.Div(id='question-container',children=current_question.Question.values[0]),
     html.Div(dcc.Input(id='input-team', type='text',placeholder="Team Name",size=35)),
     html.Div(dcc.Input(id='input-answer', type='text',placeholder="Response",size=35)),
     html.Div(dcc.Input(id='input-wager', type='text',placeholder="Wager",size=35)),
     html.Button('Submit', id='submit-button'),
-    html.Button('Refresh Question', id='question-button'),
-    html.Div(id='output-container-button',children="Let's get started!!!")
+    html.Div(id='output-container-button',children="Let's get started!!!"),
+    html.Div([dcc.Graph(id='standings-table',figure=fig)], \
+    style={'width': '50%', 'display': 'inline-block', 'padding': '0 20'}),
+    dcc.Interval(id='interval-component',interval=10*1000,n_intervals=0)
 ])
 
 @app.callback(
@@ -84,17 +88,30 @@ app.layout = html.Div([
     dash.dependencies.State('input-wager','value')])
 def update_output(n_clicks, team, answer, wager):
     if n_clicks is not None:
-        add_response(team,question_ind,answer,wager)
+        add_response(team,current_question.Number.values[0],answer,wager)
         return '"{}" just answered "{}" and wagered {}.'.format(team,answer,wager,n_clicks),'',''
 
 @app.callback(
-    dash.dependencies.Output('question-container-button', 'children'),
-    [dash.dependencies.Input('question-button','n_clicks')])
-def update_question(n_clicks):
-    global question_ind
+    [dash.dependencies.Output('question-container', 'children'),
+    dash.dependencies.Output('standings-table', 'figure')],
+    [dash.dependencies.Input('interval-component','n_intervals')])
+def update_question(n_intervals):
+    global current_question
     questions = get_values('Questions!A1:D1000')
+    responses = get_values('Responses!A1:F1000')
+    responses.Wager = responses.Wager.astype(int)
+    standings = pd.merge(left=pd.merge(left=pd.DataFrame({'Team Name':responses['Team Name'].unique()}),\
+    right=responses.loc[responses.Scoring == 'Correct'].groupby('Team Name').Wager.sum()\
+    .reset_index().rename(columns={'Wager':'Score'}),how='left',on='Team Name'),\
+    right=responses.loc[responses.Scoring == 'Unscored'].groupby('Team Name')\
+    .size().to_frame('Unscored').reset_index(),how='left',on='Team Name')
+    standings.Score = standings.Score.fillna(0)
+    standings.Unscored = standings.Unscored.fillna(0)
+    fig = go.Figure(data=[go.Table(columnwidth=[100,50,50],header=dict(values=list(standings.columns),\
+    fill_color='paleturquoise',align='left'),cells=dict(values=[standings['Team Name'],\
+    standings['Score'],standings['Unscored']],fill_color='lavender',align='left'))])
     current_question = questions.loc[questions.Active == 'TRUE']
-    return current_question['Question'].values[0]
+    return current_question['Question'].values[0], fig
 
 if __name__ == '__main__':
     app.run_server()
